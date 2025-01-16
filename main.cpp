64c70104fdeb2340a9fa6d7df5105fe3e5f0577a
a.abdumalikov <a.abdumalikov@newuu.uz>
2025-01-16 10:18:08 +0500

    hello message and age

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout<<"Hello World!"<< endl;
+    cout<<"How old are you"<<endl;
+    cout<<"Age:"<<endl;
+    int age;
+    cin>>age;
+    cout<<"your age is "<<age<<endl;
+
+    return 0;
+
+}
